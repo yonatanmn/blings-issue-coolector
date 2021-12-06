c3592a638e6e6a348eafdaaa0aa8ff2125b73d96
yonatan <yonatanmn@users.noreply.github.com>
2021-12-06 14:16:54 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,6 @@ export default function Home() {
 </ul>
       </main>
 
-//       <Footer />
     
     
     <script type="text/javascript" src="https://blingsio.atlassian.net/s/d41d8cd98f00b204e9800998ecf8427e-T/-dtzt95/b/3/c95134bc67d3a521bb3f4331beb9b804/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?jsI18nTransformer=migrated&locale=en-US&collectorId=4d22b419"></script>
