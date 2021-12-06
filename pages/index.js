f2fd4a3c3b81b0def0ff60190f345fb17e1c6c88
yonatan <yonatanmn@users.noreply.github.com>
2021-12-06 14:12:09 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,20 +10,21 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Click on top to submit a bug" />
-        <p className="description">
-          Make sure to add:    
-        </p>
-    <p>
-    
-              your name
-              application (<code>Platform</code><code>Playground</code><code>AE Extension</code> or <code>Player/Engine</code>)
-              repro steps
-              screenshots or screen recordings
-    </p>
+     <p>Make sure to add:</p>
+<ul>
+<li>your name and email,</li>
+<li>application (<code>Platform</code><code>Playground</code><code>AE Extension</code> or <code>Player/Engine</code>)</li>
+<li>repro steps</li>
+<li>screenshots or screen recordings</li>
+</ul>
       </main>
 
 //       <Footer />
+    
+    
+    <script type="text/javascript" src="https://blingsio.atlassian.net/s/d41d8cd98f00b204e9800998ecf8427e-T/-dtzt95/b/3/c95134bc67d3a521bb3f4331beb9b804/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?jsI18nTransformer=migrated&locale=en-US&collectorId=4d22b419"></script>
+
+
     </div>
   )
 }
